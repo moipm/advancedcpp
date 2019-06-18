de30d5ad96ffec155b34da4505dc493908b09816
Moises Palmero <moipalmon@gmail.com>
2019-06-18 19:59:06 +0200

    Añadido Tema 2

---
+++ 02_Exceptions/03_ExceptionBasics.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void mightGoWrong() {
+  bool error1 = true;
+  bool error2 = true;
+
+  if(error1) {
+    throw "Something went wrong.";
+  }
+  if(error2) {
+    throw string("Something else went wrong.");
+  }
+
+}
+
+void usesMightGoWrong() {
+  mightGoWrong();
+}
+
+int main() {
+  try {
+    usesMightGoWrong();
+  }
+  catch(int e) {
+    cout << "Error code: " << e << endl;
+  }
+  catch(char const* e) {
+    cout << "Error message: " << e << endl;
+  }
+  catch(string& e) {
+    cout << "String error message: " << e << endl;
+  }
+
+  cout << "Still running" << endl;
+
+  return 0;
+}
---
+++ 02_Exceptions/04_StandardExceptions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class CanGoWrong {
+public:
+  CanGoWrong() {
+    char* pMemory = new char[9999999999999];
+    delete[] pMemory;
+  }
+};
+
+int main() {
+  try {
+    CanGoWrong wrong;
+  }
+  catch(bad_alloc& e) {
+    cout << "Caught exception: " << e.what() << endl;
+  }
+
+  cout << "Still running" << endl;
+
+  return 0;
+}
